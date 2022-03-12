2594ab90ac73e88bdea149613500b4b0a3ae0f6d
assiqur191 <rahmanmdassiqur@gmail.com>
2022-03-12 21:08:29 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE >
+
+<html>
+<head>
+      <time>MY website</time>
+</head>
+<body>
+   <a href="logout.php">Logout</a>
+<h1>This is the index page </h1>
+
+<br>
+Hello, Username.
+</body>   
+   
+
+
+
+
+</html>
---
+++ loginpage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>The Delivery Boy </head>
+<h1> Login </h1>
+<html>
+<body>
+    <form>
+        <lebel for="first name" > Username : </lebel>
+<input type="test" id="fname" name="fname" placeholder="First_name" ><br>
+<lebel for="text">Last name : </lebel>
+  <input type="text" id="lname" name="lname" placeholder="Last_name" ><br>
+ <level for="pwd" >Password : </level> 
+ <input type="password" id="pwd" name="password" placeholderd="password" ><br>
+
+
+
+</form>
+</body>
+
+</html>
